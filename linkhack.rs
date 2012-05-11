80272a985fd3ec3cd6430402e25551096eb43722
Brian Anderson <banderson@mozilla.com>
2012-05-11 16:24:07 -0700

    Mac has different linking needs

--- linkhack.rs
+++ linkhack.rs
@@ -1,6 +1,13 @@
 // FIXME: This is a huge hack to find the static version of the library
 // instead of the shared. It looks in a very specific place that only has
 // relevance to servo.
+
+#[cfg(target_os = "linux")]
 #[link_args = "../harfbuzz/src/.libs/libharfbuzz.a -lpango-1.0"]
+#[no_link]
+native mod m { }
+
+#[cfg(target_os = "macos")]
+#[link_args = "../harfbuzz/src/.libs/libharfbuzz.a -lstdc++"]
 #[no_link]
 native mod m { }
