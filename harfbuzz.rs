5c0def8dadcde1d4bea1f532067f6505ac893a2c
Patrick Walton <pcwalton@mimiga.net>
2012-05-17 17:37:47 -0700

    Fix union type

--- harfbuzz.rs
+++ harfbuzz.rs
@@ -10,7 +10,7 @@ type hb_position_t = int32_t;
 
 type hb_mask_t = uint32_t;
 
-type union__hb_var_int_t = c_void /* FIXME: union type */;
+type union__hb_var_int_t = int;
 
 type hb_var_int_t = union__hb_var_int_t;
 
