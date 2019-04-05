e59f3c0bc54369a30be7e22e9b1db05e3b49f481
Sean Leather <sean.leather@gmail.com>
2019-04-05 13:38:10 +0200

    bash multiline string

--- harfbuzz-sys/touch.sh
+++ harfbuzz-sys/touch.sh
@@ -3,18 +3,17 @@
 # If the timestamp on these files is incorrect (because git does not preserve timestamps)
 # then the configure script will incorrectly try to run automake commands to regenerate them.
 # We `touch` these files to prevent this.
-AUTOMAKE_FILES = \
-  configure.ac \
-  m4/*.m4 \
-  aclocal.m4 \
-  configure \
-  config.h.in \
-  Makefile.* \
-  */Makefile.* \
-  */*/Makefile.* \
-  */*/*/Makefile.* \
-  */*/*/*/Makefile.* \
-  gtk-doc.make
+AUTOMAKE_FILES="configure.ac \
+                m4/*.m4 \
+                aclocal.m4 \
+                configure \
+                config.h.in \
+                Makefile.* \
+                */Makefile.* \
+                */*/Makefile.* \
+                */*/*/Makefile.* \
+                */*/*/*/Makefile.* \
+                gtk-doc.make"
 
 cd $(git rev-parse --show-toplevel)/harfbuzz-sys/harfbuzz
 touch -r configure $AUTOMAKE_FILES
