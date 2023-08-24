ba107cd2e29e89a5ca35a355d4ee35938dfc3474
Bruce Mitchener <bruce.mitchener@gmail.com>
2023-08-25 06:51:23 +0700

    harfbuzz-sys: Use `core` types rather than `std`. (#231)
    Some environments may want to build as `no_std` as mentioned in
    PR #197.
    
    Update the `update.sh` script to use the right `bindgen` command
    line.

--- harfbuzz-sys/update.sh
+++ harfbuzz-sys/update.sh
@@ -3,4 +3,4 @@ rm -rf harfbuzz
 tar xvf harfbuzz-2.7.4.tar.xz
 rm harfbuzz-2.7.4.tar.xz
 mv harfbuzz-2.7.4 harfbuzz
-bindgen --no-prepend-enum-name --whitelist-function hb_.\* --whitelist-type hb_.\* wrapper.h | sed 's/ f32/ ::std::os::raw::c_float/' | sed 's/u32_/u32/' | sed 's/i32_/i32/' | sed 's/u16_/u16/' | sed 's/i16_/i16/' | sed 's/u8_/u8/' | sed 's/i8_/i8/' >src/bindings.rs
+bindgen --use-core --no-prepend-enum-name --allowlist-function hb_.\* --allowlist-type hb_.\* wrapper.h | sed 's/ f32/ ::core::ffi::c_float/' | sed 's/u32_/u32/' | sed 's/i32_/i32/' | sed 's/u16_/u16/' | sed 's/i16_/i16/' | sed 's/u8_/u8/' | sed 's/i8_/i8/' >src/bindings.rs
